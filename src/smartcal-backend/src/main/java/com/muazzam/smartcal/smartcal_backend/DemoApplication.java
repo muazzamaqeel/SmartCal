4056415b1638df8a694f251fa8b6fb588f946e66
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2026-02-05 18:29:01 +0100

    Backend Setup

---
+++ src/smartcal-backend/src/main/java/com/muazzam/smartcal/smartcal_backend/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.muazzam.smartcal.smartcal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
